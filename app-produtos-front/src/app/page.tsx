a22fe1ed4ddb3693866aae6dee0827e83cf63496
xarobi <132419016+pdroluc4as@users.noreply.github.com>
2026-03-23 17:13:12 +0000

    passo 6 e 7 concluido

--- app-produtos-front/src/app/page.tsx
+++ app-produtos-front/src/app/page.tsx
@@ -1,65 +1,72 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
+import { getProdutosTodos } from "@/services/api"
 
 export default function Home() {
+  const [produtos, atualizarProdutos] = useState<any[]>([]);
+  const [tituloPesquisa, setTituloPesquisa] = useState("");
+
+  useEffect(() => {
+    getProdutosTodos().then((resultado) => {
+      atualizarProdutos(resultado.data.products);
+    })
+  }, []);
+
+  const produtosFiltrados = produtos.filter((produto) =>
+    produto.title.toLowerCase().includes(tituloPesquisa.toLowerCase())
+  );
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div>
+      <header className="mb-6">
+        <h1>Pesquisa de produtos</h1>
+        <label htmlFor="tituloPesquisa">Pesquisar por título:</label>
+        <input
+          id="tituloPesquisa"
+          type="text"
+          value={tituloPesquisa}
+          onChange={(e) => setTituloPesquisa(e.target.value)}
+          placeholder="Digite o título do produto"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+      </header>
+      
+      <main>
+        <h1 className="text-2xl font-bold text-slate-900">Produtos</h1>
+        {produtosFiltrados.length === 0 ? (
+          <p className="text-gray-500">Nenhum produto corresponde ao filtro.</p>
+        ) : (
+          <div className="flex flex-wrap gap-4 justify-center">
+            {produtosFiltrados.map((p) => (
+              <CardProduto key={p.id} produto={p} />
+            ))}
+          </div>
+        )}
       </main>
     </div>
+  );
+}
+
+type CardProdutoProps = { produto: ProdutoType };
+
+function CardProduto({ produto }: CardProdutoProps) {
+  return (
+    <article
+      className="h-120 w-120 rounded-lg border border-orange-300 border-2 p-4 shadow-sm transition hover:shadow-md"
+    >
+      {produto.thumbnail ? (
+        <img
+          className="mb-3 h-60 w-full rounded-md"
+          src={produto.thumbnail}
+          alt={produto.title}
+        />
+      ) : (
+        <div className="mb-3 h-60 w-full rounded-md bg-gray-100" />
+      )}
+
+      <h2 className="mb-1 text-lg font-semibold text-slate-900">{produto.title}</h2>
+      <p className="mb-3 text-sm text-gray-600">{produto.description}</p>
+      <p className="text-xl font-bold text-emerald-600">R$ {produto.price}</p>
+    </article>
   );
 }
